7b9f021e87c55623515bd72603c122b6972cdcea
robinn1 <RobiNN1@users.noreply.github.com>
2018-12-27 20:24:51 +0100

    Add Dutch, German

---
+++ German/defender.php
@@ -0,0 +1,26 @@
+<?php
+// Tokens
+$locale['token_error_title'] = "Token Fehler";
+$locale['token_error'] = "Es tut uns leid, da war ein Fehler. Bitte gehen Sie zur&uuml;ck, aktualisiere die Seite und versuche es erneut.";
+$locale['token_error_1'] = "Session wurde nicht gestartet.";
+$locale['token_error_2'] = "Token wurde nicht geposted.";
+$locale['token_error_3'] = "Ung&uuml;ltiger Token.";
+$locale['token_error_4'] = "Ung&uuml;ltige UserID innerhalb des Token.";
+$locale['token_error_5'] = "Ung&uuml;ltiger Token Zeitstempel.";
+$locale['token_error_6'] = "Der Post wurde zu schnell gemacht.";
+$locale['token_error_7'] = "Ung&uuml;ltiger Token Hash.";
+$locale['token_error_8'] = "Ung&uuml;ltiges Token Format.";
+
+$locale['df_400'] = "%s enth&auml;lt ung&uuml;ltige Zeichen.";
+$locale['df_401'] = "%s ist keine g&uuml;ltige E-Mail-Adresse.";
+$locale['df_402'] = "%s ist kein g&uuml;ltiges Passwort.";
+$locale['df_403'] = "%s ist keine g&uuml;ltige Zahl.";
+
+$locale['df_415'] = "Ung&uuml;ltiger Dateiname.";
+$locale['df_416'] = "Upload Datei ist zu gro&szlig;. Datei muss kleiner sein als %s.";
+$locale['df_417'] = "Upload Datei ist ein ung&uuml;ltiger Dateityp. Es werden nur %s akzeptiert.";
+$locale['df_418'] = "Upload Datei ist in Ordnung, aber die Erweiterung ist ung&uuml;ltig. Bitte benenne die Dateierweiterung um und versuche es erneut.";
+$locale['df_419'] = "Bild&uuml;berpr&uuml;fung fehlgeschlagen, bitte versuch es nochmal.";
+$locale['df_420'] = "Ordner existiert nicht. Bitte erstelle einen Ordner via FTP und versuch es nochmal.";
+$locale['df_421'] = "Bild darf nicht gr&ouml;&szlig;er sein als %uw x %uh pixels.";
+?>
